c5688030a91e5ad5be0a3e8ea93a37a969dd8a27
Yaresko Vlad <yareskofamily@gmail.com>
2022-08-16 17:39:14 +0300

    Бургер не работает

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-
 $(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+	$('.header-navbar__btn').click(function () {
+		$(this).toggleClass('.header-navbar__btn--active');
+	});
 });
